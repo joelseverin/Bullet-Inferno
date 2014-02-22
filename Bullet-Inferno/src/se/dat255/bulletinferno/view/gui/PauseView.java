eb8b0583c4acb71fe457a58d553c6cc4c2e0130c
sebbe33 <seb.blomberg@gmail.com>
2014-02-22 01:32:34 +0100

    Added background to the PauseView

--- Bullet-Inferno/src/se/dat255/bulletinferno/view/gui/PauseView.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/view/gui/PauseView.java
@@ -2,33 +2,51 @@ package se.dat255.bulletinferno.view.gui;
 
 import se.dat255.bulletinferno.util.ResourceManager;
 import se.dat255.bulletinferno.util.TextureDefinitionImpl;
+import se.dat255.bulletinferno.view.menu.MenuBackgroundView;
 import se.dat255.bulletinferno.view.menu.SimpleToggleSubMenuView;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
 
 public class PauseView extends SimpleToggleSubMenuView implements Disposable {
-	private final static int GLASS_WIDTH = 1112, GLASS_HEIGHT = 1080, VIRTUAL_WIDTH = 1920;
+	private final static int GLASS_WIDTH = 1112, GLASS_HEIGHT = 1080, VIRTUAL_HEIGHT = 1080,
+			VIRTUAL_WIDTH = 1920;
 	private final static float GLASS_ANIMATION_DURATION = 0.4f;
 	
 	private final Stage stage;
 	private final Button resumeButton, mainMenuButton, restartButton, muteButton;
 	private final Table table;
+	private final Image background;
 	
 	public PauseView(Stage stage, ResourceManager resources) {
 		super(new Group(), GLASS_WIDTH, GLASS_HEIGHT, (VIRTUAL_WIDTH - GLASS_WIDTH)/2, GLASS_HEIGHT, 
 				GLASS_ANIMATION_DURATION);
 		this.stage = stage;
 		
+		if(stage.getHeight() != VIRTUAL_HEIGHT || stage.getWidth() != VIRTUAL_WIDTH) {
+			stage.setViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, false);
+		}
+		
+		background = new Image();
+		background.setSize(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+		stage.addActor(background);
+		
 		table = new Table();
 		table.padTop(180);
+		table.setFillParent(true);
+		table.top();
+		table.setBackground(resources.getDrawableTexture(TextureDefinitionImpl.LOADOUTMENU_GLASS));
 		
-		table.add(new Image(resources.getDrawableTexture(TextureDefinitionImpl.PAUSEMENU_HEADER)));
+		table.add(new Image(resources.getDrawableTexture(TextureDefinitionImpl.PAUSEMENU_HEADER)))
+			.padTop(190);
 		table.row();
 		
 		resumeButton = new Button(
@@ -48,6 +66,7 @@ public class PauseView extends SimpleToggleSubMenuView implements Disposable {
 				resources.getDrawableTexture(TextureDefinitionImpl.PAUSEMENU_RESTART_BUTTON_DOWN));
 		table.add(restartButton).padTop(35);
 		table.row();
+		table.debug();
 		
 		muteButton = new Button(
 				resources.getDrawableTexture(TextureDefinitionImpl.MENU_SOUND_ON),
@@ -57,6 +76,14 @@ public class PauseView extends SimpleToggleSubMenuView implements Disposable {
 		
 		getToggleActor().addActor(table);
 		stage.addActor(getToggleActor());
+	}
+	
+	public void setBackground(TextureRegion texture) {
+		TextureRegionDrawable trd = new TextureRegionDrawable(texture);
+		trd.setMinHeight(VIRTUAL_HEIGHT);
+		trd.setMinWidth(VIRTUAL_WIDTH);
+		background.setDrawable(trd);
+		background.pack();
 	}
 	
 	public void addResumeButtonListener(EventListener listener) {
